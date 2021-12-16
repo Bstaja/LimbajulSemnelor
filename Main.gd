cb1bd20973a1ac4c55f219af0b302fd29fb547e2
Bstaja <aha.gamedev@gmail.com>
2021-12-16 20:57:20 +0200

    Update Main.gd

--- Main.gd
+++ Main.gd
@@ -23,7 +23,7 @@ var dictionar = {
 	"Îmbrăcăminte":"imbracaminte",
 	"Instrumente\nmuzicale":"Instrumente muzicale",
 	"Numerele":"Numerele",
-	"Prepozitii":"Prepozitii",
+	"Prepoziții":"Prepozitii",
 }
 
 #Dictionar care contine denumirile tuturor butoanelor din aplicatie
